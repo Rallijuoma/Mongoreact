a940e3365b0561b45e2419b8c0ae4c6fca867dea
Rallijuoma <huuskonen39@gmail.com>
2022-08-25 10:15:44 +0300

    kos

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const title = 'Superadvanced web phonebook app'
+  const name1 = 'John Doe'
+  const phone1 = '358401234567'
+  const name2 = 'Jane Doe'
+  const phone2 = '44551234567'
+  const name3 = 'Foo bar'
+  const phone3 = '000'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{name1} {phone1}</p>
+      <p>{name2} {phone2}</p>
+      <p>{name3} {phone3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+) 
